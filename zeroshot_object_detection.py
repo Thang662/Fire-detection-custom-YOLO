38db3bec937c98a9d1b5494b1669b0583dbdea10
Thang662 <letranthang0606@gmail.com>
2024-12-28 20:38:38 +0000

    Add transformer models

--- zeroshot_object_detection.py
+++ zeroshot_object_detection.py
@@ -4,7 +4,7 @@ from transformers import AutoProcessor, AutoModelForZeroShotObjectDetection
 from datasets import load_dataset
 from torchmetrics.detection.mean_ap import MeanAveragePrecision
 import torch
-from tqdm import tqdm_notebook as tqdm
+from tqdm import tqdm
 import argparse
 
 parser = argparse.ArgumentParser()
